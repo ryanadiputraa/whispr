4402a3a1ae46d96b7eccf82fdf3f15ed8afa8387
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-04-16 16:09:20 +0800

    fix: user error logging

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -40,6 +40,7 @@ export class UserService {
       if (error instanceof UniqueConstraintError) {
         throw new BadRequestException({ message: 'email already taken' });
       }
+      this.logger.error('error');
       throw new InternalServerErrorException();
     }
   }
@@ -56,6 +57,7 @@ export class UserService {
       return user;
     } catch (error) {
       if (error instanceof HttpException) throw error;
+      this.logger.error('error');
       throw new InternalServerErrorException();
     }
   }
@@ -71,6 +73,7 @@ export class UserService {
       return user;
     } catch (error) {
       if (error instanceof HttpException) throw error;
+      this.logger.error('error');
       throw new InternalServerErrorException();
     }
   }
